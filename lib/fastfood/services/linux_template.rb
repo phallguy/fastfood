1b6a8c3078cd06b327a9f81a4306d95f1e45de81
Paul Alexander <paul@xheo.com>
2014-06-28 21:28:59 -0700

    Clarifying LinuxTemplate

--- lib/fastfood/services/linux_template.rb
+++ lib/fastfood/services/linux_template.rb
@@ -6,20 +6,33 @@ module Fastfood
 
       private
         def run_with_data( data )
+          upload_template( data )
+          establish_permissions( data )
+        end
+
+        def upload_template( data )
           on_host do
-            path    = Fastfood.find_file( data[:template] ) \
-                      || Fastfood.find_file( File.join( "config/fastfood/templates", data[:template] ) )
-            output  = StringIO.new( ERB.new( File.read( path ) ).result(binding) )
+            output = generate_template( data )
 
             if data[:sudo]
               sudo_upload! output, data[:destination]
             else
               upload! output, data[:destination]
             end
+          end
+        end
 
+        def establish_permissions( data )
+          on_host do
             sudo :chmod, data[:chmod], data[:destination] if data[:chmod]
             sudo :chown, data[:chown], data[:destination] if data[:chown]
           end
+        end
+
+        def generate_template( data )
+          path = Fastfood.find_file( data[:template] ) \
+                      || Fastfood.find_file( File.join( "config/fastfood/templates", data[:template] ) )
+          StringIO.new( ERB.new( File.read( path ) ).result(binding) )
         end
 
     end
